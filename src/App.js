72b58d41b360a87f3fbad09a5f1ecf757997b4c5
alajido <96017433+Alajido@users.noreply.github.com>
2023-01-04 16:16:13 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Lovereact from './Lovereactjs.js';
+
+function App() {
+  return (
+    <div className="App">
+     <Info />
+     <Lovereact />
+    </div>
+  );
+}
+
+function Info() {
+  return(
+    <div>
+      <h1>Why i love react js</h1>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/Lovereactjs.js
@@ -0,0 +1,11 @@
+export default function Lovereact() {
+    return(
+      <ul>
+        <li>Popular in the industry</li>
+        <li>Easy to learn</li>
+        <li>Component reusability</li>
+        <li>performance</li>
+        <li>Flexibility</li>
+      </ul>
+    )
+  }
