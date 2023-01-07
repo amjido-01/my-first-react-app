0ffe0e330b56dfe432b144ee20b031ec2fa2fb20
alajido <96017433+Alajido@users.noreply.github.com>
2023-01-07 15:29:53 +0100

    commit

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHashtag} from '@fortawesome/free-solid-svg-icons';
+
 
 const Clock = ({time, color}) => {
+    const styles = {
+        clock: {
+            border: "2px solid white",
+            backgroundColor: "black",
+            color: "white",
+            width: "300px",
+            marginLeft: "auto",
+            marginRight: "auto",
+            textAlign: "center",
+            padding: "10px"
+        },
+        user: {
+            textAlign: "right"
+        }
+    }
    return(
-    <h1 style={{ color: color }}>
-        { time }
-    </h1>
+    <div className='clock' style={ styles.clock } >
+        <>
+        <FontAwesomeIcon icon={faHashtag} />
+
+            <h1>
+                { time }
+            </h1>
+            <p style={{ border: '1px solid white' }}>Saturday, January 07</p> 
+            <p className="user" style={ styles.user }>@amjido_01</p>
+        </>
+    </div>
    )
 }
 
