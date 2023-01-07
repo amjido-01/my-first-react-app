acb1f32964561739115204ce4fbd11874df9fb57
alajido <96017433+Alajido@users.noreply.github.com>
2023-01-07 13:13:43 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import './App.css';
 import Data from './components/Data';
 import { PropsExample } from './components/PropsExample';
 import Clock from './components/Clock';
+import { useEffect, useState } from 'react';
 // import Card from './components/Card';
 
 
+const useTime = () => {
+  const [time, setTime] = useState(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date())
+    }, 1000);
+    return () => clearInterval(id)
+  }, []);
+  return time;
+}
+
 function App() {
+  
   return (
     <div className="App">
      {/* <Card /> */}
 
+      {/* display data component */}
      <Data />
      < PropsExample name='@alajido_01' 
         person = {{
@@ -19,7 +33,8 @@ function App() {
         }} 
       />
 
-      <Clock />
+      {/* clock component */}
+      <Clock color={ color } time={ time.toLocaleTimeString() } />
 
     </div>
   );
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,15 +1,11 @@
 import React from "react";
 
-const Clock = () => {
-    const date = new Date();
-    const time = date.getHours() 
-        + ':' + date.getMinutes() 
-        + ":" + date.getSeconds();
-    return(
-        <h1 style={{ color: 'blue' }}>
+const Clock = ({time, color}) => {
+   return(
+    <h1 style={{ color: color }}>
         { time }
-      </h1>
-    )
+    </h1>
+   )
 }
 
 export default Clock;
