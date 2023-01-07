17f9c26c06dd558acfa42b7cb071ce79225a4a02
alajido <96017433+Alajido@users.noreply.github.com>
2023-01-07 12:04:31 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Data from './components/Data';
 import { PropsExample } from './components/PropsExample';
+import Clock from './components/Clock';
 // import Card from './components/Card';
 
 
@@ -8,11 +9,18 @@ function App() {
   return (
     <div className="App">
      {/* <Card /> */}
+
      <Data />
-     < PropsExample name='@alajido_01' person = {{
-        firstName: 'Abubakar',
-        age: 23
-     }} />
+     < PropsExample name='@alajido_01' 
+        person = {{
+          firstName: 'Abubakar',
+          age: 23,
+          color: 'green'
+        }} 
+      />
+
+      <Clock />
+
     </div>
   );
 }
--- src/components/PropsExample.js
+++ src/components/PropsExample.js
@@ -3,7 +3,13 @@ import React from "react";
 export const PropsExample = (props) => {
     return(
         <>
-            <h1 style = {{ color: 'red'}}>{props.name}</h1>
+            <h1 
+                style = {{ color: props.person.color}}
+                >
+                {props.name} <br/> {props.person.firstName}
+            </h1>
+
         </>
     )
 }
+
