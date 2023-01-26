337c95a49aaf4377b659ece420f452ac52bcc67c
alajido <96017433+Alajido@users.noreply.github.com>
2023-01-26 21:04:15 +0100

    commit

--- src/components/form.js
+++ src/components/form.js
@@ -2,7 +2,14 @@ import React from "react";
 
 
 function Form() {
-
+    return(
+        <>
+            <form>
+                <label ></label>
+                <input type={} id=""
+            </form>
+        </>
+    )
 }
 
 export default Form
