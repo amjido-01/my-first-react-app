5dae6efb2cec6c1a7db7e08533ff2bbdea02a817
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-05 22:14:59 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,38 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 
 
 function App() {
+  const [tasks, setTasks] = ([
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "qui est esse",
+      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
+])
   return (
     <div className="App container border-solid border-2 border-green-600 mx-auto h-64">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
-
 
 
 
