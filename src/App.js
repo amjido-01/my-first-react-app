de5cfa2f3aeaf58611a6c1bc16aae36e55990369
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-05 16:08:29 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
-import Test from './components/Test';
-
+import Tasks from './components/Tasks';
 
 
 
 function App() {
-  const onClick = () => {
-    console.log('testing')
-  }
   return (
     <div className="App container border-solid border-2 border-green-600 mx-auto h-64">
       <Header />
-      <Test text='test' color='#ca8a04' onClick={onClick}/>
+      <Tasks />
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Button = ({ color, text, onClick }) => {
   return (
-    <button onClick={ onClick } style={{backgroundColor: color}} class="bg-{ color } hover:bg-green-400 text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+    <button onClick={ onClick } style={{backgroundColor: color}} className="bg-{ color } hover:bg-green-400 text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow">
       { text }
     </button> 
   )
