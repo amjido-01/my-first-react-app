ea7b0bd77c88cfd09d5e5edfbbba57e1d5ed18d9
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-05 23:33:27 +0100

    commit

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Task = ({task}) => {
   return (
-    <div className='text-green-500 bg-gray-300 p-2 my-2 text-xl'>{task.title}</div>
+    <div className='text-green-500 bg-gray-300 p-2 my-2 text-xl cursor-pointer'>{task.title}</div>
   )
 }
 
