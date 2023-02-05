3b301af1ba44722da6344d60a8663c43e76b760e
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-05 13:56:33 +0100

    commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
-const Button = ({ color, text }) => {
+const Button = ({ color, text, onClick }) => {
   return (
-  <button style={{backgroundColor: color}} class="bg-{ color } hover:bg-green-400 text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-    { text }
-  </button>  )
+    <button onClick={onClick} style={{backgroundColor: color}} class="bg-{ color } hover:bg-green-400 text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+      { text }
+    </button> 
+  )
 }
 
 Button.prototype = {
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,13 @@ import Button from './Button';
 
 const Header = () => {
     const title = 'TASK TRACKER APP'
+    const onClick = () => {
+      console.log('clicked!')
+    }
   return (
     <div className='flex justify-between mx-auto border-solid border-2 border-sky-500 '>
       <h2 className='text-3xl '>{ title }</h2>
-      <Button color='#22c55e' text='Add task'/>
+      <Button color='#22c55e' text='Add task' onClick={onClick}/>
     </div>
   )
 }
