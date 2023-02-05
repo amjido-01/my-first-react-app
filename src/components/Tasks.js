44eaeb21228d2c70ac9a8fb0b4e5d82a793bfb9d
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-05 23:34:50 +0100

    commit

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -5,12 +5,11 @@ import Task from './Task'
 const Tasks = ({ tasks }) => {
   return (
     <div>
-        { tasks.map((task) => (
-            // <h3 key={ task.id } className='text-green-500 bg-gray-300 p-2 my-2 text-xl'> { task.title } </h3>
-         <Task key={task.id} task={task}/>
-
-        )
-        )}
+        { 
+            tasks.map((task) => (
+            <Task key={task.id} task={task}/>
+            ))
+        }
 
     </div>
   )
