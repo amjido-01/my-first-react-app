86ef49b81c90eeeb291880244f3a5e8edf3395cf
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-06 11:44:07 +0100

    commit

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Task = ({task}) => {
-    const d = new Date
+const Task = ({ task }) => {
+    const d = new Date();
   return (
     <div className='text-dark bg-gray-300 p-2 my-2 text-xl cursor-pointer'>
         <h3>{task.title}</h3>
