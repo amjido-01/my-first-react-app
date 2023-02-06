443fd4f20384bb7d8ab9c1ce65ddda274b119437
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-06 12:34:46 +0100

    commit

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,17 @@
 import React from 'react'
+// import { Fatimes } from '@fortawesome/fontawesome-svg-core'
+import { faAdd } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Task = ({ task }) => {
     const d = new Date();
   return (
     <div className='text-dark bg-gray-300 p-2 my-2 text-xl cursor-pointer'>
         <h3>{task.title}</h3>
-        <p className='text-sm'>{d.toUTCString()}</p>
+       <div className='flex'>
+            <p className='text-sm'>{d.toUTCString()}</p>
+            <faTimes style={{ color: 'red' }}/>
+       </div>
     </div>
   )
 }
